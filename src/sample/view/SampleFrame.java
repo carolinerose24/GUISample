8dd59563677bf631c5b60eb0b19582582c2a59cb
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-27 08:16:01 -0700

    Added code to sample frame class- data members and constructors

--- src/sample/view/SampleFrame.java
+++ src/sample/view/SampleFrame.java
@@ -1,6 +1,30 @@
 package sample.view;
+import javax.swing.JFrame;
+import sample.controller.GUIController;
 
-public class SampleFrame
+public class SampleFrame extends JFrame
 {
-
+	//data members
+	private GUIController appController;
+	private SamplePanel appPanel;
+	
+	//constructor-initialize data members
+	public SampleFrame(GUIController appController) //2 data members inside - controller
+	{ //first line of every constructor (in every class that uses) in extends JFrame-- must use super to call the parent structure
+		super();
+		
+		this.appController = appController; //parameter -> data member
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame(); //->send to helper method-keep constructor clean
+	}
+	
+	private void setupFrame() //helper method
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("MY first GUI App");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
