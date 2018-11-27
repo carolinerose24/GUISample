7168ee6d7ff31829b968db69474a86fb502543f8
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-27 09:10:08 -0700

    Added change background color method to button clicked action

--- src/sample/view/SamplePanel.java
+++ src/sample/view/SamplePanel.java
@@ -48,10 +48,21 @@ public class SamplePanel extends JPanel //frame holds a panel
 		{//action listeners -> are interfaces and need these structures
 			public void actionPerformed(ActionEvent mouseClick)
 			{// NEEEEEEED public void
-				
+											//System.out.println("*button click*"); //a way to see if your button is clicking
+				changeBackgroundColor();
 			}
 		});
 	}
 	
+	private void changeBackgroundColor()
+	{
+		//uses ints for color in java (swift: percentages, app inventor: lists)
+		int red = (int)(Math.random() * 256);
+		int green = (int)(Math.random() * 256); // the (int) is a CAST command -> makes it go to that form
+		int blue = (int)(Math.random() * 256); // 0 -> 255 = 256 numbers 255.234234234 -> cuts it off to make it an int
+		
+		this.setBackground(new Color (red, green, blue));
+		textLabel.setText("Red is " + red + ". Green is " + green + ". Blue is " + blue + ".");
+	}
 	
 }
